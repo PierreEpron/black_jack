4396f989c3c8e424a2323fc04431a944c0379d98
= <=>
2020-02-06 10:08:23 +0100

    Add auto shuffle when created to Deck init

--- black_jack.py
+++ black_jack.py
@@ -20,10 +20,12 @@ class Card:
 
 
 class Deck:
-    def __init__(self, base_deck, colors):
+    def __init__(self, base_deck, colors, shuffle=True):
         self.base_deck = base_deck
         self.colors = colors
         self.create()
+        if shuffle:
+            self.shuffle()
     def __str__(self):
         return '\n' + ', '.join([str(card) for card in self.deck])
 
