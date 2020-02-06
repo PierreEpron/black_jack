8677dde61bb74579a537b4e493251a11ba6c305d
= <=>
2020-02-06 13:57:43 +0100

    Work on blackjack state

--- black_jack.py
+++ black_jack.py
@@ -2,14 +2,20 @@
 
 import random
 
+
 AS = 'A'
 COLORS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 BASE_DECK = [
     ('2', 2), ('3', 3), ('4', 4), ('5', 6),
     ('6', 6), ('7', 7), ('8', 8), ('9', 9),
-    ('10', 10), ('V', 10), ('Q', 10), 
+    ('10', 10), ('J', 10), ('Q', 10), 
     ('K', 10), (AS, 1)
 ]
+BLACK_JACK = 21 
+
+LOOSE = 0
+WIN = 1
+NOTHING = 2
 
 def clear(c = 10):
     print('\n' * c)    
@@ -46,8 +52,20 @@ class Hand:
             if card.label == AS:
                 points.append(points[-1] + 10)
         return points
-
-
+    def get_best_points(self):
+        best = -1
+        for point in self.get_points():
+            if best < point <= 21:
+                best = point
+        if best == -1:
+            return self.get_min_points()
+        return best
+    def get_min_points(self):
+        min = None
+        for point in self.get_points():
+            if min == None or min > point:
+                min = point
+        return min
 class Deck:
     def __init__(self, base_deck, colors, shuffle=True):
         self.base_deck = base_deck
@@ -70,14 +88,16 @@ class Deck:
         return self.cards.pop()
 
 
-
 class Actor:
     def __init__(self):
         self.hand = Hand()
         self.name = ''
+        self.state = NOTHING
     def __str__(self):
-        return '%s : %s' % (self.name, self.hand)
-
+        return '%s : %s -> %s' % (self.name, self.hand, self.state)
+    def check_black_jack(self):
+        if BLACK_JACK == self.hand.get_best_points():
+            self.state = WIN
 
 class HumanPlayer(Actor):
     def __init__(self, name):
@@ -89,33 +109,41 @@ class Dealer(Actor):
     def __init__(self):
         Actor.__init__(self)
         self.name = 'Dealer'
+        
 
 class Game:
     def __init__(self):
         self.deck = Deck(BASE_DECK, COLORS)
-    
-    def start(self, player_count = 2):
         self.actors = []
+        self.players = []
+
+    def start(self, player_count = 2):
         self.create_dealer()
         self.create_players(player_count)
         self.first_deal()
+        self.check_black_jacks()
         self.show()
     def create_dealer(self):
         self.dealer = Dealer()
         self.actors.append(self.dealer)
     def create_players(self, player_count):
-        self.players = []
         for i in range(0, player_count):
             self.create_player(i)
-        self.actors.extend(self.players)            
-    def create_player(self, id):
-        confirm = ''
-        name = ''
-        while confirm != 'y':
-            name = input('%s player to choose a name : ' % (id+1))
-            confirm = input('Your name is %s ? (y/*)' % name)
-        self.players.append(HumanPlayer(name))
+    def create_player(self, id, name=None):
+        if name == None:
+            confirm = ''
+            while confirm != 'y':
+                name = input('%s player to choose a name : ' % (id+1))
+                confirm = input('Your name is %s ? (y/*)' % name)
+        p = HumanPlayer(name)
+        self.players.append(p)
+        self.actors.append(p)
     def first_deal(self):
         self.deck.draw_hands([actor.hand for actor in self.actors])
+    def check_black_jacks(self):
+        for actor in self.actors:
+            actor.check_black_jack()
+
     def show(self):
+        clear()
         print('\n\n'.join([str(actor) for actor in self.actors]))
